a5cc067dcaad93722a00d3801180a2f52791d28a
doasync <doasync@gmail.com>
2021-07-16 08:43:53 +0300

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
-function App() {
+import React from 'react';
+
+import logo from './logo.svg';
+
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +24,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
